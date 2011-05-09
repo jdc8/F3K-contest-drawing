147d5cfaf472e7f4a0f12fe3abd8d7a6d3cdeb23
Jos Decoster <jos.decoster@gmail.com>
2011-05-09 11:27:28 +0200

    Keep track of duels while running

--- f3ksa.cpp
+++ f3ksa.cpp
@@ -44,25 +44,28 @@ struct Contest;
 
 struct Group {
     std::vector<int> pilots;
-    void draw(const Contest*, const Round*, int);
+    void draw(const Contest*, const Round*, int, std::map<std::pair<int, int>, int>&);
     int in_group(int p) const;
     void duels(std::map<std::pair<int, int>, int>& eduels) const;
 };
 
 struct Round {
     std::vector<Group> groups;
-    void draw(const Contest*);
+    void draw(const Contest*, std::map<std::pair<int, int>, int>&);
     int in_round(int p) const;
     void duels(std::map<std::pair<int, int>, int>& eduels) const;
-    void step();
-    void step0(int p, int q);
-    int stepm(int p, int q);
-    void get_group_and_index(int p, int& pg, int& pi);
+    void remove_duels(int, int, std::map<std::pair<int, int>, int>& eduels) const;
+    void add_duels(int, int, std::map<std::pair<int, int>, int>& eduels) const;
+    void step(std::map<std::pair<int, int>, int>&);
+    void step0(int p, int q, std::map<std::pair<int, int>, int>&);
+    int stepm(int p, int q, std::map<std::pair<int, int>, int>&);
+    void get_group_and_index(int p, int& pg, int& pi) const;
     Round() {}
 };
 
 struct Contest {
     std::vector<Round> rounds;
+    std::map<std::pair<int, int>, int> cduels;
     int npilots;
     std::vector<int> group_npilots;
     int nrounds;
@@ -129,10 +132,8 @@ int Contest::sum_duel_occurences(const std::map<std::pair<int, int>, int>& eduel
 
 double Contest::cost() const 
 {
-    std::map<std::pair<int, int>, int> eduels;
-    duels(eduels);
     std::map<int, int> ov;
-    int mov = sum_duel_occurences(eduels, ov);
+    int mov = sum_duel_occurences(cduels, ov);
     double cost = 0;
     
 //     cost = cost + mov * 10e9; // don't like many duels
@@ -218,9 +219,8 @@ void Contest::report()
     for(std::vector<Round>::const_iterator I = rounds.begin(); I != rounds.end(); I++)
 	os << "round " << r++ << " {" << *I << "}\n";
     std::map<std::pair<int, int>, int> eduels;
-    duels(eduels);
     std::map<int, int> ov;
-    int mov = sum_duel_occurences(eduels, ov);
+    int mov = sum_duel_occurences(cduels, ov);
     os << "duel_frequencies";
     for(int i = 0; i <= mov; i++)
 	if (ov.count(i))
@@ -236,8 +236,8 @@ void Contest::report()
 	    std::pair<int, int> k = mangle(p, q);
 	    if (p == q)
 		os << "  -";
-	    else if (eduels.count(k))
-		os << " " << std::setw(2) << eduels.at(k);
+	    else if (cduels.count(k))
+		os << " " << std::setw(2) << cduels.at(k);
 	    else
 		os << "  0";
 	}
@@ -262,22 +262,30 @@ int Round::in_round(int p) const
     return 0;
 }
 
-void Group::draw(const Contest* contest, const Round* round, int npilots)
+void Group::draw(const Contest* contest, const Round* round, int npilots, std::map<std::pair<int, int>, int>& cduels)
 {
     pilots.clear();
     do {
 	int p = random() % contest->npilots;
-	if (!round->in_round(p) && !this->in_group(p))
+	if (!round->in_round(p) && !this->in_group(p)) {
+	    for(std::vector<int>::const_iterator I = pilots.begin(); I != pilots.end(); I++) {
+		std::pair<int,int> k = mangle(p, *I);
+		if (cduels.count(k))
+		    cduels[k]++;
+		else
+		    cduels[k] = 1;
+	    }
 	    pilots.push_back(p);
+	}
     } while (pilots.size() < npilots);
 }
 
-void Round::draw(const Contest* contest)
+void Round::draw(const Contest* contest, std::map<std::pair<int, int>, int>& cduels)
 {
     groups.clear();
     for(std::vector<int>::const_iterator I = contest->group_npilots.begin(); I != contest->group_npilots.end(); I++) {
 	Group group;
-	group.draw(contest, this, *I);
+	group.draw(contest, this, *I, cduels);
 	groups.push_back(group);
     }
 }
@@ -285,14 +293,15 @@ void Round::draw(const Contest* contest)
 void Contest::draw()
 {
     rounds.clear();
+    cduels.clear();
     for(int r = 0; r < nrounds; r++) {
 	Round round;
-	round.draw(this);
+	round.draw(this, cduels);
 	rounds.push_back(round);
     }
 }
 
-void Round::get_group_and_index(int p, int& pg, int& pi)
+void Round::get_group_and_index(int p, int& pg, int& pi) const
 {
     pg = 0;
     for(std::vector<Group>::const_iterator I = groups.begin(); I != groups.end(); I++) {
@@ -306,8 +315,31 @@ void Round::get_group_and_index(int p, int& pg, int& pi)
     }
 }
 
-void Round::step()
+void Round::remove_duels(int p, int g, std::map<std::pair<int, int>, int>& cduels) const
 {
+    for(std::vector<int>::const_iterator I = groups[g].pilots.begin(); I != groups[g].pilots.end(); I++)
+	if (*I != p) {
+	    std::pair<int,int> k = mangle(*I, p);
+	    if (cduels.count(k))
+		cduels[k]--;
+	}
+}
+
+void Round::add_duels(int p, int g, std::map<std::pair<int, int>, int>& cduels) const
+{
+    for(std::vector<int>::const_iterator I = groups[g].pilots.begin(); I != groups[g].pilots.end(); I++)
+	if (*I != p) {
+	    std::pair<int,int> k = mangle(*I, p);
+	    if (cduels.count(k))
+		cduels[k]++;
+	    else
+		cduels[k] = 1;
+	}
+}
+
+void Round::step(std::map<std::pair<int, int>, int>& cduels)
+{
+    // Swap 2 pilots from different groups
     int rg0 = random() % groups.size();
     int rg1 = rg0;
     if (groups.size() > 1) {
@@ -318,12 +350,18 @@ void Round::step()
     int rp0 = random() % groups[rg0].pilots.size();
     int rp1 = random() % groups[rg1].pilots.size();
     int p = groups[rg0].pilots[rp0];
-    groups[rg0].pilots[rp0] = groups[rg1].pilots[rp1];
+    int q = groups[rg1].pilots[rp1];
+    remove_duels(p, rg0, cduels);
+    remove_duels(q, rg1, cduels);
+    groups[rg0].pilots[rp0] = q;
     groups[rg1].pilots[rp1] = p;
+    add_duels(p, rg1, cduels);
+    add_duels(q, rg0, cduels);
 }
 
-void Round::step0(int p, int q)
+void Round::step0(int p, int q, std::map<std::pair<int, int>, int>& cduels)
 {
+    // Get random pilots from same group as p and swap it with pilot q
     int pg = 0;
     int pi = 0;
     get_group_and_index(p, pg, pi);
@@ -334,13 +372,19 @@ void Round::step0(int p, int q)
     do {
 	r = random() % groups[pg].pilots.size();
     } while (r == pi);
-    int tmp = groups[pg].pilots[r];
-    groups[pg].pilots[r] = groups[qg].pilots[qi];
-    groups[qg].pilots[qi] = tmp;
+    int rtmp = groups[pg].pilots[r];
+    int qtmp = groups[qg].pilots[qi];
+    remove_duels(rtmp, pg, cduels);
+    remove_duels(qtmp, qg, cduels);
+    groups[pg].pilots[r] = qtmp;
+    groups[qg].pilots[qi] = rtmp;
+    add_duels(rtmp, qg, cduels);
+    add_duels(qtmp, pg, cduels);
 }
 
-int Round::stepm(int p, int q)
+int Round::stepm(int p, int q, std::map<std::pair<int, int>, int>& cduels)
 {
+    // When pilots p and q are in same group, swap pilots p with pilot from other group
     int pg = 0;
     int pi = 0;
     get_group_and_index(random() % 2 ? p : q, pg, pi);
@@ -355,45 +399,48 @@ int Round::stepm(int p, int q)
     } while (rg == pg);
     
     int ri = random() % groups[rg].pilots.size();
-    int tmp = groups[pg].pilots[pi];
-    groups[pg].pilots[pi] = groups[rg].pilots[ri];
-    groups[rg].pilots[ri] = tmp;
+    int ptmp = groups[pg].pilots[pi];
+    int rtmp = groups[rg].pilots[ri];
+    remove_duels(ptmp, pg, cduels);
+    remove_duels(rtmp, rg, cduels);
+    groups[pg].pilots[pi] = rtmp;
+    groups[rg].pilots[ri] = ptmp;
+    add_duels(rtmp, pg, cduels);
+    add_duels(ptmp, rg, cduels);
     return 1;
 }
 
 void Contest::step(double u, double step_size)
 {
-    std::map<std::pair<int, int>, int> eduels;
-    duels(eduels);
     std::map<int, int> ov;
-    int mov = sum_duel_occurences(eduels, ov);
+    int mov = sum_duel_occurences(cduels, ov);
     if (mov >= max_duels) {
  	for(int i = 0; i < (u*100); i++) {
  	    int rr = random() % nrounds;
- 	    rounds[rr].step();
+ 	    rounds[rr].step(cduels);
  	}
     }
     else {
 	if (ov.count(0)) {
 	    std::vector<std::pair<int, int> > eduels0;
-	    for(std::map<std::pair<int, int>, int>::const_iterator I = eduels.begin(); I != eduels.end(); I++)
+	    for(std::map<std::pair<int, int>, int>::const_iterator I = cduels.begin(); I != cduels.end(); I++)
 		if (I->second == 0)
 		    eduels0.push_back(I->first);
 	    int i = 0;
 	    for(; i < (u * 10) && eduels0.size(); i++) {
 		int rr = random() % nrounds;
 		int r0 = random() % eduels0.size();
-		rounds[rr].step0(eduels0[r0].first, eduels0[r0].second);
+		rounds[rr].step0(eduels0[r0].first, eduels0[r0].second, cduels);
 		eduels0.erase(eduels0.begin()+r0);
 	    }
 	    for(; i < (u * 10) && eduels0.size(); i++) {
 		int rr = random() % nrounds;
-		rounds[rr].step();
+		rounds[rr].step(cduels);
 	    }
  	}
 	else {
 	    std::vector<std::pair<int, int> > eduelsm;
-	    for(std::map<std::pair<int, int>, int>::const_iterator I = eduels.begin(); I != eduels.end(); I++)
+	    for(std::map<std::pair<int, int>, int>::const_iterator I = cduels.begin(); I != cduels.end(); I++)
 		if (I->second == mov)
 		    eduelsm.push_back(I->first);
 	    int i = 0;
@@ -403,14 +450,22 @@ void Contest::step(double u, double step_size)
 		do {
 		    rr = random() % nrounds;
 		    r0 = random() % eduelsm.size();
-		} while(!rounds[rr].stepm(eduelsm[r0].first, eduelsm[r0].second));
+		} while(!rounds[rr].stepm(eduelsm[r0].first, eduelsm[r0].second, cduels));
 		eduelsm.erase(eduelsm.begin()+r0);
 	    }
 	    for(; i < (u * 10); i++) {
 		int rr = random() % nrounds;
-		rounds[rr].step();
+		rounds[rr].step(cduels);
 	    }
 	}
+    }
+
+    std::map<std::pair<int, int>, int> tduels;
+    duels(tduels);
+
+    if (cduels != tduels) {
+	std::cerr << "duel diff!!!!!" << std::endl;
+	exit(5);
     }
 }
 
@@ -436,10 +491,8 @@ void S1(const gsl_rng * r, void *xp, double step_size)
 void P1(void *xp)
 {
     Contest* c = (Contest*)xp;
-    std::map<std::pair<int, int>, int> eduels;
-    c->duels(eduels);
     std::map<int, int> ov;
-    int mov = c->sum_duel_occurences(eduels, ov);
+    int mov = c->sum_duel_occurences(c->cduels, ov);
     std::cout << "#" << c->cost();
     for(int i = 0; i <= mov; i++)
 	if (ov.count(i))
